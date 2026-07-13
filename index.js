724b1359c229b981aaa5c1b03828bdf4fa9ca151
nagleci107 <nagleci107@gmail.com>
2026-07-13 19:37:18 -0400

    fix search, reorganize code

--- index.js
+++ index.js
@@ -7,72 +7,43 @@ const t = localStorage.getItem("title");
 
 const searchInput = document.querySelector("#search-input");
 
-let movies;
-
-async function filterMovies(filter) {
-  const movieListEl = document.querySelector(".movies");
-
-  movieListEl.classList.add("movies__loading")
-
-  if (!movies) {
-    movies = await getMovies();
-  }
-
-  movieListEl.classList.remove("movies__loading");
-
-  if (filter === "ALPH_TO_Z") {
-    movies.sort((a, b) => a.title.localeCompare(b));
-  } 
-  else if (filter === "ALPH_TO_A") {
-    movies.sort((a, b) => b.title.localeCompare(a));
-  }
-  else if (filter === "NEWEST") {
-    movies.sort((a, b) => a.released.getTime() - b.released.getTime());
-  }
-  else if (filter === "OLDEST") {
-    movies.sort((a, b) => b.released.getTime() - a.released.getTime());
-  }
-};
-  
+let currentMovies = [];
 
 
 async function getMovies(t) {
+    
     const movies = await fetch(`http://www.omdbapi.com/?s=${t}&apikey=1d08b643`);
     const moviesData = await movies.json();
+    
+    if (moviesData.Response === "True") {
+            currentMovies = moviesData.Search; // Store the current movie results
+            await renderMovies(currentMovies);
+        } else {
+            movieListEl.innerHTML = `<div>No results found for "${t}"</div>`;
+        }
+}
+
+async function renderMovies(movies) {
+
+    movieListEl.classList.add("movies__loading")
+
+    if (!currentMovies) {
+        currentMovies = await getMovies();
+    }
+
+    movieListEl.classList.remove("movies__loading");
 
     movieListEl.innerHTML = ""; // Clear previous results
 
-    // Check if the 'Search' array exists
-    if (moviesData.Response === "True") {
-        const movieDetailsPromises = moviesData.Search.map(async (movie) => {
-            const detailResponse = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=1d08b643`);
-            return await detailResponse.json();
-
-            console.log(detailResponse)
-        });
-
+    const movieDetailsPromises = movies.map(async (movie) => {
+        const detailResponse = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=1d08b643`);
+        return await detailResponse.json();
+    });
         const detailedMovies = await Promise.all(movieDetailsPromises);
-        movieListEl.innerHTML = detailedMovies.map(movieHTML).join("");
-    } 
-    else if (moviesData.Response === "False") {
-        movieListEl.innerHTML = `<div>No results found for "${t}"</div>`;
-        return;
-    }
-    else {
-        movieListEl.innerHTML = `<p>No movies found.</p>`;
-    }
-
-    moviesData.Search.forEach(movie => {
+        detailedMovies.forEach(movie => {
         movieListEl.innerHTML += movieHTML(movie);
     });
 }
-
-// waits for search query to be entered to re-render
-searchInput.addEventListener("input", async () => {
-    const t = searchInput.value;
-    await getMovies(t);
-});
-
 
 function movieHTML(movie) {
     const rottenTomatoes = movie.Ratings.find(rating => rating.Source === "Rotten Tomatoes");
@@ -101,18 +72,44 @@ function movieHTML(movie) {
   </div>`;
 }
 
-// function filterRatings() {
-//     const allSources = ["Internet Movie Database", "Rotten Tomatoes", "Metacritic"]
-//     const targetSource = "RottenTomatoes"
+async function filterMovies(event) {
+    function parseReleaseDate(releaseDate) {
+    const [day, month, year] = releaseDate.split(' ');
+    const monthIndex = {
+        Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
+        Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11
+    }[month];
+    return new Date(year, monthIndex, day);
+    }
 
-// }
 
-// function filterMovies(event) {
-//   renderMovies(event.target.value);
-// }
+    if (event === "ALPH_TO_Z") {
+    currentMovies.sort((a, b) => a.Title.localeCompare(b.Title));
+  } 
+  else if (event === "ALPH_TO_A") {
+    currentMovies.sort((a, b) => b.Title.localeCompare(a.Title));
+  }
+  else if (event === "NEWEST") {
+    currentMovies.sort((a, b) => parseReleaseDate(b.Released) - parseReleaseDate(a.Released));
+  }
+  else if (event === "OLDEST") {
+    currentMovies.sort((a, b) => parseReleaseDate(a.Released) - parseReleaseDate(b.Released));
+  }
+
+  await renderMovies(currentMovies);
+};
+
+// waits for search query to be entered to re-render
+searchInput.addEventListener("keydown", async (event) => {
+    if (event.key === "Enter") { 
+        const t = searchInput.value; 
+        await getMovies(t);
+    }
+});
 
 
 
 setTimeout(() => {
-  getMovies();
-});
+    const initialTitle = localStorage.getItem("title") || "";
+    getMovies(initialTitle);
+}, 1000);
