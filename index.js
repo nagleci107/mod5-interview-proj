8ea72d6c49ca36f41b2dae58ccba9426f94d2cce
nagleci107 <nagleci107@gmail.com>
2026-07-13 17:41:14 -0400

    add scripting for search, fix HTML and styling

--- index.js
+++ index.js
@@ -1,52 +1,105 @@
 // OMDb API http://www.omdbapi.com/?i=tt3896198&apikey=1d08b643
 // key: 1d08b643
 
+const movieListEl = document.querySelector(".movies");
 
-// async function renderMovies(filter) {
-//   const moviesWrapper = document.querySelector(".movies");
+const t = localStorage.getItem("title");
 
-//   moviesWrapper.classList.add("movies__loading")
+const searchInput = document.querySelector("#search-input");
 
-//   if (!movies) {
-//     movies = await getMovies();
-//   }
+let movies;
 
-//   moviesWrapper.classList.remove("movies__loading");
+async function filterMovies(filter) {
+  const movieListEl = document.querySelector(".movies");
 
-//   if (filter === "ALPH_TO_Z") {
-//     movies.sort((a, b) => a.title.localeCompare(b));
-//   } 
-//   else if (filter === "ALPH_TO_A") {
-//     movies.sort((a, b) => b.title.localeCompare(a));
-//   }
-//   else if (filter === "NEWEST") {
-//     movies.sort((a, b) => a.released.getTime() - b.released.getTime());
-//   }
-//   else if (filter === "OLDEST") {
-//     movies.sort((a, b) => b.released.getTime() - a.released.getTime());
-//   }
+  movieListEl.classList.add("movies__loading")
 
- 
+  if (!movies) {
+    movies = await getMovies();
+  }
 
-//   const moviesHtml = movies.map((movie) => {
-//     return `<div class="movie__title">
-            // ${movie.title}
-            // </div>
-            // <div class="movie__released">
-            //  ${movie.released}
-            // </div>
-            // <div class="movie__runtime">
-            //   ${movie.runtime}
-            // </div>
-            // <div class="movie__tomatoes">
-            //   ${movie.ratings}
-            // </div>
-//   </div>`;
-//   })
-//   .join("");
+  movieListEl.classList.remove("movies__loading");
 
-//   moviesWrapper.innerHTML = moviesHtml
-// }
+  if (filter === "ALPH_TO_Z") {
+    movies.sort((a, b) => a.title.localeCompare(b));
+  } 
+  else if (filter === "ALPH_TO_A") {
+    movies.sort((a, b) => b.title.localeCompare(a));
+  }
+  else if (filter === "NEWEST") {
+    movies.sort((a, b) => a.released.getTime() - b.released.getTime());
+  }
+  else if (filter === "OLDEST") {
+    movies.sort((a, b) => b.released.getTime() - a.released.getTime());
+  }
+};
+  
+
+
+async function getMovies(t) {
+    const movies = await fetch(`http://www.omdbapi.com/?s=${t}&apikey=1d08b643`);
+    const moviesData = await movies.json();
+
+    movieListEl.innerHTML = ""; // Clear previous results
+
+    // Check if the 'Search' array exists
+    if (moviesData.Response === "True") {
+        const movieDetailsPromises = moviesData.Search.map(async (movie) => {
+            const detailResponse = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=1d08b643`);
+            return await detailResponse.json();
+
+            console.log(detailResponse)
+        });
+
+        const detailedMovies = await Promise.all(movieDetailsPromises);
+        movieListEl.innerHTML = detailedMovies.map(movieHTML).join("");
+    } 
+    else if (moviesData.Response === "False") {
+        movieListEl.innerHTML = `<div>No results found for "${t}"</div>`;
+        return;
+    }
+    else {
+        movieListEl.innerHTML = `<p>No movies found.</p>`;
+    }
+
+    moviesData.Search.forEach(movie => {
+        movieListEl.innerHTML += movieHTML(movie);
+    });
+}
+
+// waits for search query to be entered to re-render
+searchInput.addEventListener("input", async () => {
+    const t = searchInput.value;
+    await getMovies(t);
+});
+
+
+function movieHTML(movie) {
+    const rottenTomatoes = movie.Ratings.find(rating => rating.Source === "Rotten Tomatoes");
+
+    return `<div class="movie">
+            <div class="movie__info">
+            <figure class="movie__img--wrapper">
+            <img class="movie__img" src="${movie.Poster}" alt="${movie.Title} Poster">
+            </figure>
+            <div class="movie__title">
+            ${movie.Title}
+            </div>
+            <div class="movie__released">
+             ${movie.Released}
+            </div>
+            <div class="movie__details">
+            <div class="movie__runtime">
+              ${movie.Runtime}
+            </div>
+            <div class="movie__tomatoes">
+              ${rottenTomatoes ? rottenTomatoes.Value : 'No rating available'}
+            </div>
+            </div>
+            </div>
+            </div>
+  </div>`;
+}
 
 // function filterRatings() {
 //     const allSources = ["Internet Movie Database", "Rotten Tomatoes", "Metacritic"]
@@ -60,6 +113,6 @@
 
 
 
-// setTimeout(() => {
-//   renderMovies();
-// });
+setTimeout(() => {
+  getMovies();
+});
